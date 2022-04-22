057e34fdbeb8e900d509b0bc95a57ee858cf67a0
FengJunJian <446187960@qq.com>
2022-04-22 16:36:58 +0800

    class names

--- DatasetClass.py
+++ DatasetClass.py
@@ -1008,3 +1008,20 @@ ImageNet_className={0: 'tench, Tinca tinca',
  997: 'bolete',
  998: 'ear, spike, capitulum',
  999: 'toilet tissue, toilet paper, bathroom tissue'}
+
+Ship_classNames=['passenger ship',#1
+                'ore carrier',#2
+                'general cargo ship',#3
+                'fishing boat',#4
+                'Sail boat',#5
+                'Kayak',#6
+                'flying bird',#flying bird/plane #7
+                'vessel',#vessel/ship #8
+                'Buoy',#9
+                'Ferry',#10
+                'container ship',#11
+                'Other',#12
+                'Boat',#13
+                'Speed boat',#14
+                'bulk cargo carrier',#15
+                ]
